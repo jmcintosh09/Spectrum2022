8e3a2f6a1d62ffbcb6f4275b813a30cfa1878d39
Jasmine McIntosh <jdm0037@auburn.edu>
2022-11-03 00:58:17 -0500

    first commit

--- main.php
+++ main.php
@@ -4,6 +4,7 @@ $rootdirectory = "/xampp/htdocs/Spectrum2022";
 require($rootdirectory."/_includes/config.php");
 
 	$headerpath = $rootdirectory."/_includes/header-pushdown.php";
+    $titleimagepath = $rootdirectory."/images/spectrum_logo_black.png";
 
 	/* PAGE & SPECIFIC METADATA - These are basic page metadata variables that can be customized for each page; These can also be updated on your config file for site-wide usage */
 
@@ -18,43 +19,16 @@ require($rootdirectory."/_includes/config.php");
 include ($headerpath);
 
 ?>
+	
 
-	<style>
-		
-		/* PAGE SPECIFIC STYLING */
-		.title-container {background:url("https://auburn.edu/main/students/studentlife/_images/studlife_header_w.jpg"); background-position: center center; background-size:cover;}
-		
-		
-		@media (min-width:576px) {
-		}
-		
-		@media (min-width:768px) {
-			
-		}
-		
-		@media (min-width:992px) {
-		}
-		
-		@media (min-width:1200px) {
-		}
-		
-		@media (min-width:1400px) {
-		}
-		
-				
-	</style>	
-
-	<div id="title_container" class="title-container row align-items-center title-bluebar" role="banner" aria-labelledby="page_title">
+	<div id="title_container" class="title-container row align-items-center" role="banner" aria-labelledby="page_title">
 
 		<div class="content col row align-items-center">
 			<div class="col">
-				<h1 id="page_title" class="title">Unit Title Here</h1>
+                
+				<h1 id="page_title" class="title">Spectrum Educational Tool</h1>
 			</div>
 			<div class="angle-right"></div>
-		</div>
-
-		<div class="content-feature-slant photo">
-			<div class="slant-container"></div>
 		</div>
 
 	</div>
@@ -76,27 +50,27 @@ include ($headerpath);
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 	  <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
 		<li class="nav-item">
-		  <a class="nav-link" href="#">Item #1</a>
+		  <a class="nav-link" href="#">Services</a>
 		</li>
 		<li class="nav-item">
-		  <a class="nav-link" href="#">Item #2</a>
+		  <a class="nav-link" href="#">Case Studies</a>
 		</li> 
 		<li class="nav-item dropdown">
           <a id="navbarDropdown01" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Item #3
+            Life Studies
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown01">
-			<li><a class="dropdown-item" href="#">Dropdown Item #1</a></li>
-			  <li><a class="dropdown-item" href="#">Dropdown Item #2</a></li>
-			  <li><a class="dropdown-item" href="#">Dropdown Item #3</a></li>
-			  <li><a class="dropdown-item" href="#">Dropdown Item #4</a></li>
+			<li><a class="dropdown-item" href="#">Chapter 1</a></li>
+			  <li><a class="dropdown-item" href="#">Chapter 2</a></li>
+			  <li><a class="dropdown-item" href="#">Chapter 3</a></li>
+			  <li><a class="dropdown-item" href="#">Chapter 4</a></li>
           </ul>
         </li>   
 		<li class="nav-item">
-		  <a class="nav-link" href="#">Item #4</a>
+		  <a class="nav-link" href="#">Tools</a>
 		</li>
 		<li class="nav-item">
-		  <a class="nav-link" href="#">Item #5</a>
+		  <a class="nav-link" href="#">Contact Us</a>
 		</li>
 		<li class="nav-item dropdown">
           <a id="navbarDropdown02" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -118,29 +92,13 @@ include ($headerpath);
 
 		<div class="content_row row">
 
-			<h2 class="section-header">H2 Section Header</h2>
+			<h2 class="section-header">Spectrum Educational Tool</h2>
 
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. Id consectetur purus ut faucibus pulvinar. A pellentesque sit amet porttitor eget dolor. Vitae semper quis lectus nulla at volutpat diam ut venenatis. Ac tincidunt vitae semper quis lectus. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Proin fermentum leo vel orci porta non pulvinar. Accumsan tortor posuere ac ut consequat semper viverra nam. Commodo quis imperdiet massa tincidunt nunc. Dictum at tempor commodo ullamcorper. Neque volutpat ac tincidunt vitae semper quis lectus nulla at. Et malesuada fames ac turpis egestas.</p>
+            <p>Innovative teacher training introduces a new set of demanding challenges and issues. Teaching has become a very complex and demanding process. Teachers with little exposure to digital media and with little preparation with digital media may face difficulties in dealing with the complexity of classroom teacing. These new teachers may often find the demands of the classroom too challenging and quckily become discourages or adopt a simplisitc view of teaching where 'Tricks & Trade' serve as a substitute for thoughtful process.</p>
 
-			<p>Nulla posuere sollicitudin aliquam ultrices sagittis orci a. Condimentum vitae sapien pellentesque habitant morbi tristique. Vitae proin sagittis nisl rhoncus. Risus nec feugiat in fermentum posuere urna nec tincidunt. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis. Maecenas volutpat blandit aliquam etiam. Sed vulputate mi sit amet. Tincidunt praesent semper feugiat nibh sed pulvinar. Justo eget magna fermentum iaculis eu. Magna ac placerat vestibulum lectus mauris ultrices eros. Tellus molestie nunc non blandit massa. Penatibus et magnis dis parturient montes nascetur ridiculus. Aliquet nec ullamcorper sit amet risus nullam. In fermentum et sollicitudin ac orci. Suscipit tellus mauris a diam maecenas sed enim ut sem. Vitae justo eget magna fermentum iaculis eu.</p>
+			<h2 class="section-header">Why Spectrum Educational Tool?</h2>
 
-			<h2 class="section-header">H2 Section Header</h2>
-
-			<p>Aliquet eget sit amet tellus cras adipiscing. Ut sem nulla pharetra diam sit amet nisl suscipit adipiscing. Cras pulvinar mattis nunc sed blandit libero volutpat. Congue mauris rhoncus aenean vel elit. Tellus orci ac auctor augue mauris augue. Eu scelerisque felis imperdiet proin fermentum. Sagittis eu volutpat odio facilisis. Tincidunt lobortis feugiat vivamus at augue. Proin fermentum leo vel orci porta non pulvinar neque laoreet. Accumsan tortor posuere ac ut consequat semper viverra. Neque gravida in fermentum et sollicitudin ac. Laoreet sit amet cursus sit amet dictum sit. Gravida neque convallis a cras semper. Eu ultrices vitae auctor eu augue ut lectus. In massa tempor nec feugiat nisl pretium fusce id.</p>
-
-			<p>Auctor elit sed vulputate mi sit amet. Aliquet lectus proin nibh nisl condimentum id venenatis a. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. A diam sollicitudin tempor id eu nisl nunc. Arcu non sodales neque sodales ut etiam. Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. Malesuada fames ac turpis egestas maecenas. Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nunc non blandit massa enim nec dui. Amet massa vitae tortor condimentum lacinia quis vel eros donec. Amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et. Viverra tellus in hac habitasse platea. Mi eget mauris pharetra et ultrices neque ornare. Habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat. Est ante in nibh mauris cursus mattis molestie a iaculis.</p>
-
-			<h3 class="section-header">H3 Section Header</h3>
-
-			<p>Aliquet eget sit amet tellus cras adipiscing. Ut sem nulla pharetra diam sit amet nisl suscipit adipiscing. Cras pulvinar mattis nunc sed blandit libero volutpat. Congue mauris rhoncus aenean vel elit. Tellus orci ac auctor augue mauris augue. Eu scelerisque felis imperdiet proin fermentum. Sagittis eu volutpat odio facilisis. Tincidunt lobortis feugiat vivamus at augue. Proin fermentum leo vel orci porta non pulvinar neque laoreet. Accumsan tortor posuere ac ut consequat semper viverra. Neque gravida in fermentum et sollicitudin ac. Laoreet sit amet cursus sit amet dictum sit. Gravida neque convallis a cras semper. Eu ultrices vitae auctor eu augue ut lectus. In massa tempor nec feugiat nisl pretium fusce id.</p>
-
-			<p>Auctor elit sed vulputate mi sit amet. Aliquet lectus proin nibh nisl condimentum id venenatis a. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. A diam sollicitudin tempor id eu nisl nunc. Arcu non sodales neque sodales ut etiam. Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. Malesuada fames ac turpis egestas maecenas. Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nunc non blandit massa enim nec dui. Amet massa vitae tortor condimentum lacinia quis vel eros donec. Amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et. Viverra tellus in hac habitasse platea. Mi eget mauris pharetra et ultrices neque ornare. Habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat. Est ante in nibh mauris cursus mattis molestie a iaculis.</p>
-
-			<h4 class="section-header">H4 Section Header</h4>
-
-			<p>Aliquet eget sit amet tellus cras adipiscing. Ut sem nulla pharetra diam sit amet nisl suscipit adipiscing. Cras pulvinar mattis nunc sed blandit libero volutpat. Congue mauris rhoncus aenean vel elit. Tellus orci ac auctor augue mauris augue. Eu scelerisque felis imperdiet proin fermentum. Sagittis eu volutpat odio facilisis. Tincidunt lobortis feugiat vivamus at augue. Proin fermentum leo vel orci porta non pulvinar neque laoreet. Accumsan tortor posuere ac ut consequat semper viverra. Neque gravida in fermentum et sollicitudin ac. Laoreet sit amet cursus sit amet dictum sit. Gravida neque convallis a cras semper. Eu ultrices vitae auctor eu augue ut lectus. In massa tempor nec feugiat nisl pretium fusce id.</p>
-
-			<p>Nulla posuere sollicitudin aliquam ultrices sagittis orci a. Condimentum vitae sapien pellentesque habitant morbi tristique. Vitae proin sagittis nisl rhoncus. Risus nec feugiat in fermentum posuere urna nec tincidunt. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis. Maecenas volutpat blandit aliquam etiam. Sed vulputate mi sit amet. Tincidunt praesent semper feugiat nibh sed pulvinar. Justo eget magna fermentum iaculis eu. Magna ac placerat vestibulum lectus mauris ultrices eros. Tellus molestie nunc non blandit massa. Penatibus et magnis dis parturient montes nascetur ridiculus. Aliquet nec ullamcorper sit amet risus nullam. In fermentum et sollicitudin ac orci. Suscipit tellus mauris a diam maecenas sed enim ut sem. Vitae justo eget magna fermentum iaculis eu.</p>
+            <p>By using Spectrum Education tool, it provides extensive training of teachers which will help them in understanding digital media and know better strategies for successful classroom management. The teachers will be well versed with teaching skills, instructional strategies, curricula and assessment practices. Our goal is to reinforce best practices for classroom management and to create an online learning environment to support pre-service and in-dervice teachers.</p>
 
 
 	</div>
